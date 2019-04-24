e31e49d24c4499227cccbe20d70365fac3b4b6ff
Spencer Hewitt <spencerhewitt@dhcp-38-179.mil.secure.int.wireless.unca.edu>
2019-04-24 12:14:14 -0400

    added shape arraylist

--- shapeFun/src/shapeFun/shapeFun.java
+++ shapeFun/src/shapeFun/shapeFun.java
@@ -1,11 +1,18 @@
 package shapeFun;
 
+import java.util.ArrayList;
+
 public class shapeFun {
 
 	public static void main(String[] args) {
+		ArrayList<Shape> shapeList = new ArrayList<> ();
 		Shape myShape = new Shape(1, "SomeShape");
 		Rectangle myRectangle = new Rectangle(2, "Rectangle 1");
 		Square mySquare = new Square(3,"Square 1");
+		
+		shapeList.add(myShape);
+		shapeList.add(myRectangle);
+		shapeList.add(mySquare);
 		
 		System.out.println(myShape);
 		System.out.println(myRectangle);
@@ -14,5 +21,9 @@ public class shapeFun {
 		mySquare.setWidth(5);
 		
 		System.out.printf("Area of %s = %1.2f \n", mySquare.getName(), mySquare.area());
+		
+		for (Shape shape : shapeList) {
+			System.out.println(shape);
+		}
 	}
 }
